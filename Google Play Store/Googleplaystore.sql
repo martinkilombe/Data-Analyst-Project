42f7f129bdbf87ae4244b05f9667320097b1e1c5
Martin Muti Kilombe <martinkilombe@gmail.com>
2023-05-23 10:51:25 +0300

    New sql changes

--- Google Play Store/Googleplaystore.sql
+++ Google Play Store/Googleplaystore.sql
@@ -1,8 +1,27 @@
-/*Total Number of rows*/
+/*Total Number of empty Rating rows*/
 SELECT COUNT(*) as total_rows,
 Count("Rating") as non_empty_rows,
 (Count(*)) - COUNT("Rating") AS empty_rows,
-(COUNT("Rating") / COUNT(*)::float) * 100 AS percentage_empty
+(COUNT(*) - COUNT("Rating")::float)*100/COUNT(*) as percentage_empty_rows
 FROM googleplaystore
 
+/*Total Number of empty Reviews rows-- No empty Rows*/
+SELECT COUNT(*) as total_rows,
+Count("Reviews") as non_empty_rows,
+(Count(*)) - COUNT("Reviews") AS empty_rows,
+(COUNT(*) - COUNT("Reviews")::float)*100/COUNT(*) as percentage_empty_rows
+FROM googleplaystore
 
+/*Total Number of empty Category rows-- No empty Rows*/
+SELECT COUNT(*) as total_rows,
+Count("Category") as non_empty_rows,
+(Count(*)) - COUNT("Category") AS empty_rows,
+(COUNT(*) - COUNT("Category")::float)*100/COUNT(*) as percentage_empty_rows
+FROM googleplaystore
+
+/*Total Number of empty Rating rows*/
+SELECT COUNT(*) as total_rows,
+Count("Rating") as non_empty_rows,
+(Count(*)) - COUNT("Rating") AS empty_rows,
+(COUNT(*) - COUNT("Rating")::float)*100/COUNT(*) as percentage_empty_rows
+FROM googleplaystore
